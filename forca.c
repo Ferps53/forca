63dc42441b1608fca945c7590198be8cbefd7b3a
Matheus Ris <matheusriys@gmail.com>
2025-02-15 14:39:57 -0300

    Incrementado o jogo (imperfeito, mas funcional);

--- forca.c
+++ forca.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <threads.h>
+#include <time.h>
+
 
 #define LIMITE_DE_PALAVRAS 10
 #define ARQUIVO_PALAVRAS "palavras.bin"
@@ -86,7 +87,6 @@ void escrever_palavras() {
   if (string == NULL) {
     return;
   }
-
   Palavra palavra = criar_nova_palavra(string);
 
   // Adiciona no final do vetor do struct
@@ -149,12 +149,83 @@ char *ler_palavra_usuario() {
   if (palavra == NULL) {
     return NULL;
   }
-
   // Evita espaços nas palavras do jogo
   scanf(" %s", palavra);
   return palavra;
 }
 
+int gerar_numero_da_palavra_aleatoria() {
+  srand(time(NULL));
+  int numero_aleatorio = rand() % 11;
+  do{
+    if (numero_aleatorio > palavras.quantidade_atual -1) {
+      numero_aleatorio = rand() % 11;
+    }
+  }while (numero_aleatorio > palavras.quantidade_atual -1);
+  printf("%d\n", numero_aleatorio);
+  printf("Quantidade atual de palavras: %d\n", palavras.quantidade_atual);
+  return numero_aleatorio;
+}
+
+Palavra definir_palavra_aleatoria() {
+  int numero_da_palavra = gerar_numero_da_palavra_aleatoria();
+  Palavra palavra_secreta = palavras.palavras_arquivo[numero_da_palavra];
+  printf("%s\n", palavra_secreta.string);
+  //Palavra aleatória definida
+  return palavra_secreta;
+}
+
+void o_jogo() {//Sim, você perdeu
+  Palavra palavra_secreta = definir_palavra_aleatoria();
+  printf("Essa palavra eh a passada pro jogo:%s\n", palavra_secreta.string);
+  int tamanho_da_palavra = strlen(palavra_secreta.string);
+  char palpite_palavra[tamanho_da_palavra];
+  for (int i = 0; i < tamanho_da_palavra; i++) {
+    palpite_palavra[i] = '_';
+  }
+  printf("Hora de Adivinhar. Você tem 6 tentativas...\n");
+  printf("%s\n", palpite_palavra);
+
+  char letras_usadas[26];
+  int contador_letras_usadas = 0;
+  letras_usadas[0] = '\0';
+  if (letras_usadas[0] == '\0') {
+    printf("Letras Usadas: Nenhuma letra usada ainda... \n");
+  }
+  int contador_de_tentativas = 6;
+  while (contador_de_tentativas > 0) {
+    char palpite_letras;
+    printf("Advinhe uma letra da palavra: ");
+    scanf(" %c", &palpite_letras);
+    letras_usadas[contador_letras_usadas] = palpite_letras;
+    contador_letras_usadas++;
+
+    int acertou = 0;
+    for (int i = 0; i < tamanho_da_palavra; i++) {
+      if (palpite_letras == palavra_secreta.string[i]) {
+        palpite_palavra[i] = palpite_letras;
+        acertou = 1;
+      }
+    }
+    if (acertou == 0) {
+      contador_de_tentativas--;
+      printf("Você ainda tem %d tentativas...\n", contador_de_tentativas);
+    }else {
+      printf("Muito bem!\n");
+    }
+    if (strcmp(palpite_palavra, palavra_secreta.string) == 0) {
+      printf("Meus parabéns! Você ganhou!\n", palpite_palavra);
+    }
+    printf("%s\n", palpite_palavra);
+    printf("Letras usadas: ");
+    for (int i = 0; i < contador_letras_usadas; i++) {
+      printf("%c ", letras_usadas[i]);
+    }
+    printf("\n");
+  }
+  printf("Você Perdeu! Acabaram suas tentativas!\n");
+  printf("A palavra era: %s.\n\n", palavra_secreta.string);
+}
 int main() {
   ler_palavras_arquivo();
   setlocale(LC_ALL, "Portuguese");
@@ -165,7 +236,10 @@ int main() {
     escolha_menu = escolha_menu_principal();
     switch (escolha_menu) {
 
-    case 1:
+    case 1: {
+      o_jogo();
+      break;
+    }
     case 4:
     case 5:
     case 6:
